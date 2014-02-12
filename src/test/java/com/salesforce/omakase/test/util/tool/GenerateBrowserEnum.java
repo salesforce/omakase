8ed2c733ba2894040f6b9902859f6c3375fde635
Nathan McWilliams <nmcwilliams@salesforce.com>
2014-02-12 14:27:19 -0500

    fix browser enum generation

--- src/test/java/com/salesforce/omakase/test/util/tool/GenerateBrowserEnum.java
+++ src/test/java/com/salesforce/omakase/test/util/tool/GenerateBrowserEnum.java
@@ -19,6 +19,7 @@ package com.salesforce.omakase.test.util.tool;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.salesforce.omakase.data.Browser;
 import com.salesforce.omakase.data.Prefix;
@@ -30,6 +31,8 @@ import java.net.URLConnection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Handles updating the {@link Browser} enum.
@@ -53,6 +56,12 @@ public class GenerateBrowserEnum {
         URLConnection connection = new URL(BROWSERS_ENDPOINT).openConnection();
         connection.setUseCaches(false);
         Map map = new ObjectMapper().readValue(connection.getInputStream(), Map.class);
+
+        String earliestString = (String)Iterables.get(((Map)map.get("eras")).keySet(), 0);
+        Matcher matcher = Pattern.compile("e-([0-9]+)").matcher(earliestString);
+        if (!matcher.matches()) throw new RuntimeException("json format from source has changed!");
+        int earliest = Integer.parseInt(matcher.group(1));
+
         Map agents = (Map)map.get("agents");
 
         List<BrowserInfo> browsers = Lists.newArrayList();
@@ -67,15 +76,15 @@ public class GenerateBrowserEnum {
         Map iosSafari = (Map)agents.get("ios_saf");
         Map operaMini = (Map)agents.get("op_mini");
 
-        browsers.add(new BrowserInfo("ie", "IE", "Internet Explorer", Prefix.MS, versions(ie)));
-        browsers.add(new BrowserInfo("opera", "OPERA", "Opera", Prefix.O, versions(opera)));
-        browsers.add(new BrowserInfo("chrome", "CHROME", "Chrome", Prefix.WEBKIT, versions(chrome)));
-        browsers.add(new BrowserInfo("safari", "SAFARI", "Safari", Prefix.WEBKIT, versions(safari)));
-        browsers.add(new BrowserInfo("firefox", "FIREFOX", "Firefox", Prefix.MOZ, versions(firefox)));
-        browsers.add(new BrowserInfo("android", "ANDROID", "Android Browser", Prefix.WEBKIT, versions(android)));
-        browsers.add(new BrowserInfo("ie_mob", "IE_MOBILE", "IE Mobile", Prefix.MS, versions(ieMobile)));
-        browsers.add(new BrowserInfo("ios_saf", "IOS_SAFARI", "Safari on iOS", Prefix.WEBKIT, versions(iosSafari)));
-        browsers.add(new BrowserInfo("op_mini", "OPERA_MINI", "Opera Mini", Prefix.O, versions(operaMini)));
+        browsers.add(new BrowserInfo("ie", "IE", "Internet Explorer", Prefix.MS, versions(ie, earliest)));
+        browsers.add(new BrowserInfo("opera", "OPERA", "Opera", Prefix.O, versions(opera, earliest)));
+        browsers.add(new BrowserInfo("chrome", "CHROME", "Chrome", Prefix.WEBKIT, versions(chrome, earliest)));
+        browsers.add(new BrowserInfo("safari", "SAFARI", "Safari", Prefix.WEBKIT, versions(safari, earliest)));
+        browsers.add(new BrowserInfo("firefox", "FIREFOX", "Firefox", Prefix.MOZ, versions(firefox, earliest)));
+        browsers.add(new BrowserInfo("android", "ANDROID", "Android Browser", Prefix.WEBKIT, versions(android, earliest)));
+        browsers.add(new BrowserInfo("ie_mob", "IE_MOBILE", "IE Mobile", Prefix.MS, versions(ieMobile, earliest)));
+        browsers.add(new BrowserInfo("ios_saf", "IOS_SAFARI", "Safari on iOS", Prefix.WEBKIT, versions(iosSafari, earliest)));
+        browsers.add(new BrowserInfo("op_mini", "OPERA_MINI", "Opera Mini", Prefix.O, versions(operaMini, earliest)));
 
         SourceWriter writer = new SourceWriter();
 
@@ -87,11 +96,11 @@ public class GenerateBrowserEnum {
         writer.write();
     }
 
-    private String versions(Map browser) {
+    private String versions(Map browser, int indexOfCurrent) {
         List<String> all = (List<String>)browser.get("versions");
         List<Double> filtered = Lists.newArrayList();
 
-        for (int i = 0; i < all.size() - 2; i++) { // skip the last two, as they are "future" versions
+        for (int i = 0; i <= indexOfCurrent; i++) { // skip the last two, as they are "future" versions
             if (all.get(i) != null) {
                 for (String s : Splitter.on("-").split(all.get(i))) filtered.add(Double.valueOf(s));
             }
