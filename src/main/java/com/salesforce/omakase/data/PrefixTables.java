3c62909fde6427556d5062f26afe9d4a9a4e8be7
Eric Perret <eperret@salesforce.com>
2022-10-04 17:18:38 -0700

    fixes

--- src/main/java/com/salesforce/omakase/data/PrefixTables.java
+++ src/main/java/com/salesforce/omakase/data/PrefixTables.java
@@ -474,6 +474,7 @@ public final class PrefixTables {
         ImmutableTable.Builder<String, Browser, Double> builder = ImmutableTable.builder();
 
         builder.put("selection", Browser.FIREFOX, 61.0);
+        builder.put("placeholder", Browser.IE, 11.0);
         builder.put("placeholder", Browser.EDGE, 17.0);
         builder.put("placeholder", Browser.OPERA, 43.0);
         builder.put("placeholder", Browser.CHROME, 56.0);
